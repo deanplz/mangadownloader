6a2737164f285c00712fdf5ef7b16a277ef3c9b5
deanplz <dean.pellizer@yahoo.com>
2020-04-29 15:58:15 +0100

    MR-14 Created Cli tests Created error paths for Cli tests

--- src/main/scala/mangaDownloader/services/cli/Cli.scala
+++ src/main/scala/mangaDownloader/services/cli/Cli.scala
@@ -2,11 +2,18 @@ package mangaDownloader.services.cli
 
 class Cli() {
 
-  def parseInput(input: List[String]): Either[Throwable, (String, Int, Int)] = input match {
+import Cli._
+  def parseInput(input: List[String]): Either[CliError, (String, Int, Int)] = input match {
     case List(x) => Right((x, 0, 0))
     case List(x, y) => Right((x, y.toInt, 0))
     case List(x, y, z) => Right((x, y.toInt, z.toInt))
-    case _ => Left(new Throwable("bad input"))
+    case _ => Left(CliError("bad input"))
   }
 
+}
+
+object Cli {
+  sealed trait CliResult
+  case object CliSuccess extends CliResult
+  case class CliError(msg: String) extends Exception with CliResult
 }

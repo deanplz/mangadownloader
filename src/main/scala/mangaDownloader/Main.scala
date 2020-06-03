976fc4c503a1f34f4dc9cc2501f79044f1b7d36a
VasicJernej <jernej.vasic@gmail.com>
2020-06-03 17:29:23 +0100

    MR-21: refactor crawler and image downloader
    this is to scale to all images for chapter, rather than just
    the first one.

--- src/main/scala/mangaDownloader/Main.scala
+++ src/main/scala/mangaDownloader/Main.scala
@@ -2,6 +2,8 @@ package mangaDownloader
 import mangaDownloader.services.cli.Cli
 import mangaDownloader.services.cli.Cli.CliSuccess
 import mangaDownloader.services.downloader.crawler.Crawler
+import mangaDownloader.services.downloader.crawler.Crawler.CrawlerSuccess
+import mangaDownloader.services.downloader.imageDownloader.ImageDownloader
 
 object Main {
 
@@ -10,7 +12,24 @@ object Main {
     cli.parseInput(args.toList) match {
       case Right(CliSuccess(a, b, c)) => {
         val crawler = new Crawler
-        println(crawler.images(a))
+        crawler.images(a) match {
+          case CrawlerSuccess(totalChapters, imagesUrls) => {
+            val id = new ImageDownloader
+
+
+            imagesUrls foreach {
+              case (image, chapter, page) => {
+                println(s"image link: $image belonging to chapter $chapter")
+                id.downloadFile(image, (a, chapter, page))
+              }
+
+            }
+
+
+          }
+
+        }
+
       }
     }
 
